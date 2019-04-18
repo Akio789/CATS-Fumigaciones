cbe3951eaba840d99cff63a500b1d161ef59db45
DominCrux <49165161+DominCrux@users.noreply.github.com>
2019-04-18 16:13:52 -0500

    Create ConsultClient.java

---
+++ WEB-INF/classes/ConsultClient.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.util.ArrayList;
+import pojos.Client;;
+
+@WebServlet("/consultClient")
+public class ConsultClient extends HttpServlet {
+
+    public void init(ServletConfig config) {
+        try {
+            super.init(config);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            // Initial parameters
+            String db = getServletContext().getInitParameter("database");
+            String user = getServletContext().getInitParameter("username");
+            String pass = getServletContext().getInitParameter("password");
+
+            // JDBC
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost/" + db + "?useSSL=false&allowPublicKeyRetrieval=true";
+            Connection con = DriverManager.getConnection(url, user, pass);
+            Statement stat = con.createStatement();
+            String clientToConsult = request.getParameter("clientToConsult");
+            String sql = "SELECT * FROM Cliente WHERE nombre='" + clientToConsult + "';";
+            ResultSet res = stat.executeQuery(sql);
+
+            ArrayList<Client> client = new ArrayList<>();
+
+            // Iterate through ResultSet
+            while (res.next()) {
+                Client newClient = new Client();
+                newClient.setId(res.getInt("id"));
+                newClient.setNombre(res.getString("nombre"));
+                newClient.setDireccion(res.getString("direccion"));
+                newClient.setCorreo(res.getString("correo"));
+                newClient.setTelefono(res.getString("telefono"));
+
+                client.add(newClient);
+            }
+
+            // Save users in session
+            HttpSession session = request.getSession(false);
+            session.setAttribute("client", client);
+
+            // Determine page to dispatch to
+            RequestDispatcher disp = getServletContext().getRequestDispatcher("/consultProduct.jsp");
+
+            stat.close();
+            con.close();
+
+            if (disp != null) {
+                disp.forward(request, response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

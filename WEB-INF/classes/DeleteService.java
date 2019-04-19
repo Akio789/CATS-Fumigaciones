d93af22fff3ebfb690b9b69d26934396a1bccda2
Jorge Figueroa Peart <48333166+JorgeFigueroaP@users.noreply.github.com>
2019-04-18 20:50:00 -0500

    Create DeleteService.java

---
+++ WEB-INF/classes/DeleteService.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/deleteService")
+public class DeleteService extends HttpServlet {
+
+    public void init(ServletConfig config) {
+        try {
+            super.init(config);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            // Initial parameters
+            String db = getServletContext().getInitParameter("database");
+            String user = getServletContext().getInitParameter("username");
+            String pass = getServletContext().getInitParameter("password");
+
+            String productToDeleteId = request.getParameter("serviceToDeleteId");
+
+            // JDBC
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost/" + db + "?useSSL=false&allowPublicKeyRetrieval=true";
+            Connection con = DriverManager.getConnection(url, user, pass);
+            Statement stat = con.createStatement();
+            String sql = "DELETE FROM Servicio WHERE id='" + serviceToDeleteId + "';";
+            stat.executeUpdate(sql);
+
+            stat.close();
+            con.close();
+
+            RequestDispatcher disp = getServletContext().getRequestDispatcher("/services");
+
+            if (disp != null) {
+                disp.forward(request, response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

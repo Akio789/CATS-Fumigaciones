e459706bdba0cfc6c7ff5b4590a26b18b17f6736
Jorge Figueroa Peart <48333166+JorgeFigueroaP@users.noreply.github.com>
2019-04-19 12:56:07 -0500

    Update ConnectionServices.java

--- WEB-INF/classes/ConnectionServices.java
+++ WEB-INF/classes/ConnectionServices.java
@@ -51,7 +51,7 @@ public class ConnectionServices extends HttpServlet {
             Statement stat2 = con2.createStatement();
             String sql2 = "SELECT * FROM Cliente;";
             ResultSet res2 = stat.executeQuery(sql2);
-            ArrayList<Client> Clients = new ArrayList<>();
+            ArrayList<Client> clients = new ArrayList<>();
 
             // Iterate through ResultSet
             while (res2.next()) {
@@ -66,16 +66,43 @@ public class ConnectionServices extends HttpServlet {
             }
 
             for (int i = 0; i < services.size(); i++) {
-                for (int j = 0; j < Clients.size(); j++) {
-                    if (services.get(i).getIdCliente() == Clients.get(j).getId()) {
-                        services.get(i).setNombreCliente(Clients.get(j).getNombre());
+                for (int j = 0; j < clients.size(); j++) {
+                    if (services.get(i).getIdCliente() == clients.get(j).getId()) {
+                        services.get(i).setNombreCliente(clients.get(j).getNombre());
                     }
                 }
             }
 
             HttpSession session = request.getSession();
-            session.setAttribute("Clients", Clients);
+            session.setAttribute("Clients", clients);
 
+            Connection con3 = DriverManager.getConnection(url, user, pass);
+            Statement stat3 = con3.createStatement();
+            String sql3 = "SELECT * FROM Fumigador;";
+            ResultSet res3 = stat.executeQuery(sql3);
+            ArrayList<Fumigator> fumigators = new ArrayList<>();
+
+            // Iterate through ResultSet
+            while (res3.next()) {
+                Fumigator newFumigator = new Fumigator();
+                newFumigator.setId(res3.getInt("id"));
+                newFumigator.setNombre(res3.getString("nombre"));
+                newFumigator.setDireccion(res3.getString("direccion"));
+                newFumigator.setCorreo(res3.getString("correo"));
+
+                fumigators.add(newFumigator);
+            }
+
+            for (int i = 0; i < services.size(); i++) {
+                for (int j = 0; j < fumigators.size(); j++) {
+                    if (services.get(i).getIdFumigador() == fumigators.get(j).getId()) {
+                        services.get(i).setNombreFumigador(fumigators.get(j).getNombre());
+                    }
+                }
+            }
+
+            HttpSession session = request.getSession();
+            session.setAttribute("Fumigators", fumigators);
             // Save users in session
             request.setAttribute("services", services);
 
@@ -84,6 +111,10 @@ public class ConnectionServices extends HttpServlet {
 
             stat.close();
             con.close();
+            stat2.close();
+            con2.close();
+            stat3.close();
+            con3.close();
 
             if (disp != null) {
                 disp.forward(request, response);
